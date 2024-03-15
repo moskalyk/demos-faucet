7e20e0cada0ba8e215d5e8aa9ee25ba095f68350
moskalyk <morgan.moskalyk@protonmail.ch>
2024-03-15 14:49:38 -0400

    added pnpm to github actions

---
+++ src/env.example.js
@@ -0,0 +1,19 @@
+const ENV = {
+  walletConnectId: '',
+  appleClientId: "",
+    projectAccessKey: '',
+    waasConfigKey: '',
+    projectId: 18,
+    identityPoolId: "",
+    emailClientId: "",
+    googleClientId: '',
+    idpRegion: "",
+    rpcServer: "https://next-waas.sequence.app",
+    kmsRegion: "us-east-2",
+    emailRegion: "us-east-2",
+    keyId: ""
+  }
+  
+  module.exports = {
+    ENV
+  }
